cab4d44c932c4e0af8fb9ae34e6dba4f2206809c
ahadnur <ahadnur44@gmail.com>
2021-10-10 12:28:24 +0600

    bootstrap form added in caseform

--- src/component/CreateCase/caseForm3.js
+++ src/component/CreateCase/caseForm3.js
@@ -1,4 +1,4 @@
-import { Container, Grid, Typography, Button } from "@material-ui/core";
+import { Container, Grid, Typography } from "@material-ui/core";
 import React from "react";
 import "../Styles/createcase/caseform.css";
 import { Form } from "react-bootstrap";
@@ -84,25 +84,6 @@ export const CaseForm3 = ({ display }) => {
             </Grid>
             
           </Grid>
-         
-          
-          
-          {/* <Grid container spacing={2} style={{ margin: "20px 0", display: "flex", justifyContent: "center", alignItems: "center"}}>
-                        <Grid item md={6}>
-                            <Typography variant="body2">Additional files (permission)</Typography>
-                            <input type="text" style={{padding: "5px", backgroundColor: 'transparent', border: '1px solid white', outline: 'none', color: 'white'}} />
-                        </Grid>
-                        <Grid item md={6} style={{ margin: "20px 0" }}>
-                            <input type="file" />
-                        </Grid>
-                    </Grid>  */}
-
-          {/* <Button variant="contained" style={{ marginTop: 20, marginLeft: 20, backgroundColor:'#ffc478' }}>Add File</Button>
-
-                    <div style={{float: 'left'}}>
-                        <Button variant="contained" style={{ marginTop: 20, marginLeft: 20, backgroundColor:'#ffc478' }}>Return</Button>
-                        <Button variant="contained" style={{ marginTop: 20, marginLeft: 20, backgroundColor:'#ffc478' }}>Open a new file</Button>
-                    </div> */}
         </div>
       </Grid>
     </Container>
